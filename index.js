14a6bc83ec79fc7c44788545ba327db2ccf63557
Dubois Nicolas <dubois.nicolas@free.fr>
2018-06-16 10:27:40 +0200

    New version

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 module.exports = function(sails) {
 
-    var setup = require('./lib/setup.js');
-    var init = require('./lib/init.js');
-    var exec = require('./lib/exec.js');
+	var setup = require('./lib/setup.js');
+	var init = require('./lib/init.js');
+	//var exec = require('./lib/exec.js');
+	var music = require('./lib/music/index.js');
 
-    gladys.on('ready', function() {
-        init();
-    });
+	gladys.on('ready', function() {
+		init();
+	});
 
-    return {
-        setup,
-        init,
-        exec,
-    };
+	return {
+		setup ,
+		init ,
+		//exec, 
+		music
+	};
 };
